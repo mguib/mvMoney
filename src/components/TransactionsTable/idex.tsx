53c2d24d4a8885494ad04e21cfdc567731cd15ef
Marcos Guibson <mgss.lic17@uea.edu.br>
2021-12-23 11:10:20 -0400

    Finalizando aplicação adicionando context

--- src/components/TransactionsTable/idex.tsx
+++ src/components/TransactionsTable/idex.tsx
@@ -1,6 +1,14 @@
+import { useContext } from "react";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
+
+
+
 export function TransactionsTable(){
+    const { transactions } = useTransactions();
+  
+
   return(
     <Container>
       <table>
@@ -14,26 +22,27 @@ export function TransactionsTable(){
         </thead>
 
         <tbody>
-          <tr>
-            <td >Desenvolvimento de WebSite</td>
-            <td className="deposit">R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$ 1.100</td>
-            <td>Casa</td>
-            <td>01/12/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de WebSite</td>
-            <td>R$ 12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR',{
+                  style: 'currency',
+                  currency:'BRL'
+                }).format(transaction.amount)} 
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
   );
 }
+
+
